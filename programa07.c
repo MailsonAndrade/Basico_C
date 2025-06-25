5cd00719158faa99b330f970f06aaaa041beefc2
Mailson <mailsonandrade666@gmail.com>
2025-06-25 06:53:43 -0300

    tabuada do 6

---
+++ programa07.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    for(i = 0; i < 11; i++)
+       printf("\n%4d x 6 = %4d", i , i*6);
+
+     printf("\n");
+     system("PAUSE");
+     return 0;
+
+}
